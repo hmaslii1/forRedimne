7a74779d1903fdb4164fc42ff26f04e2bb436b80
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-08-19 09:39:11 +0000

    [Studio] Add screen "Home"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,19 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
 import { logindarkblue_PhonePortrait } from '../pages/logindarkblue/PhonePortrait/logindarkblue';
+import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 export class Screens {
   static declarations = [
     MenuComponent,
-    logindarkblue_PhonePortrait
+    logindarkblue_PhonePortrait,
+    home_PhonePortrait
   ];
   static mapping = {
     'logindarkblue': {
       PhonePortrait: logindarkblue_PhonePortrait
+    },
+    'home': {
+      PhonePortrait: home_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,3 @@
+export { mock_home as home } from './home/index';
 export { mock_logindarkblue as logindarkblue } from './logindarkblue/index';
 export { mock_menu as menu } from './menu/index';
